bb690be24f71f3d179acfa5d6a29edd3e811ff3b
Kahe-Mikaya <kahe.gohan@gmail.com>
2024-09-21 09:25:21 -0300

    criando o css de cadastro

--- crud/paginas/forms.py
+++ crud/paginas/forms.py
@@ -5,6 +5,16 @@ class CursoForm(forms.ModelForm):
     class Meta:
         model = Curso 
         fields = ['titulo', 'descricao', 'image'] # ou'__all__'
+        widgets = {
+            'titulo': forms.TextInput(attrs={'class': 'form-control' }),
+            'descricao': forms.Textarea(attrs={'class': 'form-control', 'rows': 5 }),
+            'image': forms.FileInput(attrs={'class': 'form-control-file'}),
+        }
+        labels = {
+            'titulo': 'Título do Curso',
+            'descricao': 'Descrição do Curso',
+            'image': 'Imagem do Curso',
+        }
 
 class AulaForm(forms.ModelForm):
     class Meta:
